6c1229b7a25941b2b0eab89686e28aca28328791
KirinIchiban <curious-el@yandex.ru>
2025-09-26 22:05:25 +0300

    initial commit

--- lab1/include/solve.hpp
+++ lab1/include/solve.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include <typeinfo>
 #include <limits>
 
 int find_target_height(int up, int down, int target);
--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
 #include "../include/solve.hpp"
 
 int main() {
     int UpSPeed, DownSpeed, desiredHeigth;
     
-    std::cout<<"Enter UpSPeed, DownSpeed, desiredHeigth using space button"<<std::endl;
+    std::cout<<"Enter UpSPeed, DownSpeed, desiredHeigth"<<std::endl;
     std::cin >> UpSPeed >> DownSpeed >> desiredHeigth;
     if (std::cin.fail()) {
-        std::cin.clear();
         std::cerr << "Error: wrong input type (expected integers)" << std::endl;
         std::exit(1); 
     }
+    
     std::cout<<find_target_height(UpSPeed, DownSpeed, desiredHeigth)<<std::endl;
 
     return 0;
