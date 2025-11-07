2c02f7ede73fc57d427cb5ee3a06f205715e5be2
KirinIchiban <curious-el@yandex.ru>
2025-11-07 23:27:15 +0300

    build directory remove

--- lab2/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Irelix/2curse/OOP_MAI/lab2
-# Build directory: C:/Users/Irelix/2curse/OOP_MAI/lab2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(GTests "C:/Users/Irelix/2curse/OOP_MAI/lab2/build/my-tests.exe")
-set_tests_properties(GTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Irelix/2curse/OOP_MAI/lab2/CMakeLists.txt;25;add_test;C:/Users/Irelix/2curse/OOP_MAI/lab2/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
--- lab3/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Irelix/2curse/OOP_MAI/lab3
-# Build directory: C:/Users/Irelix/2curse/OOP_MAI/lab3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(GTests "C:/Users/Irelix/2curse/OOP_MAI/lab3/build/my-tests.exe")
-set_tests_properties(GTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Irelix/2curse/OOP_MAI/lab3/CMakeLists.txt;25;add_test;C:/Users/Irelix/2curse/OOP_MAI/lab3/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
